e1a815ab85612782ae03a6626c29a7cd1d8a6dbe
Krastan Dimitrov <krastan.kdd@gmail.com>
2021-09-15 20:35:27 -0500

    Updating webpack config to support jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js(x?)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
       },
